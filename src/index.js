b1211a95e6e0eb5882b96da1337ebcaa79bb1423
Leangiordano <lea2914@gmail.com>
2022-05-19 23:20:32 -0300

    Firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXomr8yECxgrcNlI8OwEkEQv_Z9iK6jcE",
+  authDomain: "react-giordano.firebaseapp.com",
+  projectId: "react-giordano",
+  storageBucket: "react-giordano.appspot.com",
+  messagingSenderId: "173342717138",
+  appId: "1:173342717138:web:357da37f7e6b1e15a7290b",
+  measurementId: "G-3N2YD9N5GC"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/ItemListContainer/ItemListContainer.jsx
+++ src/pages/ItemListContainer/ItemListContainer.jsx
@@ -4,24 +4,51 @@ import "./ItemListContainer.styles.css";
 import { useState, useEffect } from 'react';
 import {getProducts} from '../../services/getData';
 import { useParams } from 'react-router-dom';
+import { collection, getDocs, getFirestore, query } from 'firebase/firestore';
 
 const ItemListContainer = ({greeting}) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const { categoryId } = useParams();
 
-
-  useEffect(() =>{
+  useEffect(() => {
     setLoading(true);
     getProducts (categoryId)
     .then(res => {
       setProducts(res);
     })
-    .catch(err => {
-      console.log(err);
-    })
-    .finally(() => setLoading(false));
+    const db = getFirestore();
+
+    const itemCollection = collection(db, 'items');
+
+    const q = query(
+      itemCollection
+    );
+
+    getDocs(q)
+      .then(snapshot => {
+        console.log(snapshot.docs.map(doc => { 
+          setLoading(false);
+          return { ...doc.data(), id: doc.id }
+        }));
+      })
+      .catch(
+        err => console.error(err)
+      );
   }, [categoryId]);
+
+
+ // useEffect(() =>{
+ //   setLoading(true);
+ //   getProducts (categoryId)
+ //   .then(res => {
+ //     setProducts(res);
+ //   })
+ //   .catch(err => {
+ //     console.log(err);
+ //   })
+ //   .finally(() => setLoading(false));
+ // }, [categoryId]);
 
 
   return (
