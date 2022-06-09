ca3cc59c679d7dad892adbb59bac01dd0e34d26a
Leangiordano <lea2914@gmail.com>
2022-06-08 22:11:56 -0300

    foto en cart

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useCartContext } from '../../CartContext/CartContextProvider';
 
 const CartItem = ({item}) => {
+    const {image, title} = item;
     const { deleteById, removeOneUnit } = useCartContext()
     return (
         <div>
             <h1>{item.title}</h1>
+            <img className="w-25" src= {image} alt= {title} />
             <h4>Unidades: {item.quantity}</h4>
             <h4>Precio unitario: ${item.price}</h4>
             <button className="btn-primary m-2" onClick={() => removeOneUnit(item.id)}>Eliminar 1</button>
